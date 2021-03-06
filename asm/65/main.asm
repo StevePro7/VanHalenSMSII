703211aae7acfc680d2d32976d8a790bb31bf3e6
StevePro7 <steveprozune@hotmail.com>
2021-03-06 16:18:47 +0000

    no message

--- asm/65/main.asm
+++ asm/65/main.asm
@@ -38,7 +38,7 @@
 ; screen
 .include "screen/none_screen.inc"
 .include "screen/splash_screen.inc"
-; .include "screen/title_screen.inc"
+.include "screen/title_screen.inc"
 ; .include "screen/scroll_screen.inc"
 ; .include "screen/select_screen.inc"
 ; .include "screen/record_screen.inc"
@@ -55,7 +55,7 @@ map " " to "~" = 0
 .enda
 
 Message:
-.asc "Hello Test27"
+.asc "Hello Test28"
 .db $ff
 .ends
 
