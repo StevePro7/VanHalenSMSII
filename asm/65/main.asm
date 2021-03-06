47a67f4de6fd814c3e9fe1ec5477f5215c073869
StevePro7 <steveprozune@hotmail.com>
2021-03-06 12:31:17 +0000

    no message

---
+++ asm/65/main.asm
@@ -0,0 +1,191 @@
+;==============================================================
+; WLA-DX banking setup
+;==============================================================
+.memorymap
+defaultslot 0
+slotsize $8000
+slot 0 $0000
+.endme
+
+.rombankmap
+bankstotal 1
+banksize $8000
+banks 1
+.endro
+
+;==============================================================
+; SMS defines
+;==============================================================
+.define VDPControl $bf
+.define VDPData $be
+.define VRAMWrite $4000
+.define CRAMWrite $c000
+
+;==============================================================
+; SDSC tag and SMS rom header
+;==============================================================
+.sdsctag 1.2,"Hello World!","SMS programming tutorial program - enhanced version","Maxim"
+
+.bank 0 slot 0
+.org $0000
+;==============================================================
+; Boot section
+;==============================================================
+.section "Boot" force
+boot:
+    di              ; disable interrupts
+    im 1            ; Interrupt mode 1
+    jp main         ; jump to main program
+.ends
+
+.org $0066
+;==============================================================
+; Pause button handler
+;==============================================================
+    ; Do nothing
+    retn
+
+;==============================================================
+; Main program
+;==============================================================
+.section "Main control structure" free
+main:
+    ld sp, $dff0
+
+    ;==============================================================
+    ; Set up VDP registers
+    ;==============================================================
+    ld hl,VDPInitData
+    ld b,VDPInitDataEnd-VDPInitData
+    ld c,VDPControl
+    otir
+
+    ;==============================================================
+    ; Clear VRAM
+    ;==============================================================
+    ; 1. Set VRAM write address to $0000
+    ld hl,$0000 | VRAMWrite
+    call SetVDPAddress
+    ; 2. Output 16KB of zeroes
+    ld bc,$4000     ; Counter for 16KB of VRAM
+-:  xor a
+    out (VDPData),a ; Output to VRAM address, which is auto-incremented after each write
+    dec bc
+    ld a,b
+    or c
+    jr nz,-
+
+    ;==============================================================
+    ; Load palette
+    ;==============================================================
+    ; 1. Set VRAM write address to CRAM (palette) address 0
+    ld hl,$0000 | CRAMWrite
+    call SetVDPAddress
+    ; 2. Output colour data
+    ld hl,PaletteData
+    ld bc,PaletteDataEnd-PaletteData
+    call CopyToVDP
+
+    ;==============================================================
+    ; Load tiles (font)
+    ;==============================================================
+    ; 1. Set VRAM write address to tile index 0
+    ld hl,$0000 | VRAMWrite
+    call SetVDPAddress
+    ; 2. Output tile data
+    ld hl,FontData              ; Location of tile data
+    ld bc,FontDataSize          ; Counter for number of bytes to write
+    call CopyToVDP
+
+    ;==============================================================
+    ; Write text to name table
+    ;==============================================================
+    ; 1. Set VRAM write address to tilemap index 0
+    ld hl,$3800 | VRAMWrite
+    call SetVDPAddress
+    ; 2. Output tilemap data
+    ld hl,Message
+-:  ld a,(hl)
+    cp $ff
+    jr z,+
+    out (VDPData),a
+    xor a
+    out (VDPData),a
+    inc hl
+    jr -
++:
+
+    ; Turn screen on
+    ld a,%01000000
+;          ||||||`- Zoomed sprites -> 16x16 pixels
+;          |||||`-- Doubled sprites -> 2 tiles per sprite, 8x16
+;          ||||`--- Mega Drive mode 5 enable
+;          |||`---- 30 row/240 line mode
+;          ||`----- 28 row/224 line mode
+;          |`------ VBlank interrupts
+;          `------- Enable display
+    out (VDPControl),a
+    ld a,$81
+    out (VDPControl),a
+
+    ; Infinite loop to stop program
+-:  jr -
+.ends
+
+;==============================================================
+; Helper functions
+;==============================================================
+.section "Helper functions" free
+SetVDPAddress:
+; Sets the VDP address
+; Parameters: hl = address
+    push af
+        ld a,l
+        out (VDPControl),a
+        ld a,h
+        out (VDPControl),a
+    pop af
+    ret
+
+CopyToVDP:
+; Copies data to the VDP
+; Parameters: hl = data address, bc = data length
+; Affects: a, hl, bc
+-:  ld a,(hl)    ; Get data byte
+    out (VDPData),a
+    inc hl       ; Point to next letter
+    dec bc
+    ld a,b
+    or c
+    jr nz,-
+    ret
+.ends
+
+;==============================================================
+; Data
+;==============================================================
+.section "Text section" free
+.asciitable
+map " " to "~" = 0
+.enda
+
+Message:
+.asc "Hello World!"
+.db $ff
+.ends
+
+.section "Content section"
+PaletteData:
+.db $00,$3f ; Black, white
+PaletteDataEnd:
+
+; VDP initialisation data
+VDPInitData:
+.db $04,$80,$00,$81,$ff,$82,$ff,$85,$ff,$86,$ff,$87,$00,$88,$00,$89,$ff,$8a
+VDPInitDataEnd:
+.ends
+
+.section "Font include" free
+FontData:
+.incbin "font.bin" fsize FontDataSize
+.ends
